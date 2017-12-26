657a78624cbe02684529005b340b6e850f17d55b
xieliming2017 <xieliming2006@hotmail.com>
2017-12-26 11:51:10 -0500

    checkpoin 43 completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
     root 'welcome#index'
 
-  devise_for :users
+    devise_for :users
+
+    get '/users/:id', :to => 'users#show', :as => :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
